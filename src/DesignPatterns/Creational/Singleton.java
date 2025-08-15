261334c68d97d230d86d1daeba64295787129db3
heisastark <98yashasvi@gmail.com>
2025-08-15 16:35:45 +0530

    Thread Safe Singleton

--- src/DesignPatterns/Creational/Singleton.java
+++ src/DesignPatterns/Creational/Singleton.java
@@ -7,8 +7,26 @@ class Controller{
     }
     public static Controller getInstance(){
         if(instance==null){
-            System.out.println("Creating new instance, since it doesn't exist!");
+            System.out.println("Creating new controller instance, since it doesn't exist!");
             instance=new Controller();
+        }
+        return instance;
+    }
+}
+
+class ThreadController{
+    private static volatile ThreadController instance;
+    private ThreadController(){
+
+    }
+    public static ThreadController getInstance(){
+        if(instance==null){
+            synchronized (Singleton.class){
+                if(instance==null){
+                    System.out.println("Creating new thread controller instance, since it doesn't exist!");
+                    instance=new ThreadController();
+                }
+            }
         }
         return instance;
     }
@@ -19,5 +37,8 @@ public class Singleton {
         Controller c1 = Controller.getInstance();
         Controller c2 = Controller.getInstance();
         Controller c3 = Controller.getInstance();
+
+        ThreadController tc1 = ThreadController.getInstance();
+        ThreadController tc2 = ThreadController.getInstance();
     }
 }
